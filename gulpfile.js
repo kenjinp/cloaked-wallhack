04e9ea27488e0c2956e3c71d56baf90efdfe6a0f
Kenneth <kenjin.p@gmail.com>
2015-04-15 15:41:06 +0200

    things

--- gulpfile.js
+++ gulpfile.js
@@ -79,12 +79,14 @@ gulp.task('htmlpage', function() {
 
 gulp.task('buildScript', function() {
   return browserify({
-    entries: [scriptDir + '/'+ appEntryPoint], debug: true
+    entries: ['./src/scripts/lib/reddit.min.js' , scriptDir + '/'+ appEntryPoint], debug: true
   })
     .transform(reactify)
     .bundle()
     .pipe(source(appEntryPoint))
     .pipe(gulp.dest(buildDir))
+    //.pipe(stripDebug())
+    //.pipe(uglify())
     .pipe(notify('Bundling Done.'));
 });
 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,99 @@
-var React = require('react');
+global.jQuery = global.$ = require('jquery');
+var React = require('react'),
+    imagesLoaded = require('imagesloaded');
 var name = 'Quotes From Tamriel';
+var quote = {};
+var redditData = {
+  title:'',
+  author:'',
+  url:''
+};
 
-var Content = React.createClass({
+var Quote = React.createClass({
+  loadQuoteFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      sucess: function(data) {
+        //after returned data
+        this.setState({data: data});
+        console.log('ajaxing');
+        console.log(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    //initial data
+    return {data: quote};
+  },
+  componentDidMount: function(){
+    this.loadQuoteFromServer();
+  },
   render: function() {
     return (
       <div>
-        {name}, brought to you by React!
+        {this.state.data}
       </div>
     );
   }
 });
 
-React.render(<Content />,
+var Content = React.createClass({
+  loadBackgroundFromReddit: function() {
+    var that = this;
+    reddit.hot('skyrimporn').limit(100).fetch(function (res) {
+      function grabRandom (res) {
+        var randomInt = Math.floor(Math.random() * res.data.children.length);
+        var postData = res.data.children[randomInt].data;
+        var extList = postData.url.split('.');
+        var ext = extList[extList.length - 1];
+        if ( ext !== 'jpg') {
+          return grabRandom(res);
+        } else {
+          return postData;
+        }
+      }
+      redditData = grabRandom(res);
+      $('img').prop('src', redditData.url);
+      $('img').imagesLoaded(function() {
+        $('img').show();
+        that.setState({data: redditData});
+      });
+    });
+  },
+  onEnter: function() {
+    var that = this;
+    $(window).keypress(function(e) {
+      if (e.keyCode == 0 || e.keyCode == 32) {
+        that.loadBackgroundFromReddit();
+      }
+    });
+  },
+  getInitialState: function() {
+    return {data: redditData};
+  },
+  componentDidMount: function() {
+    $('img').hide();
+    this.onEnter();
+    this.loadBackgroundFromReddit();
+  },
+  render: function() {
+    return (
+      <div>
+        <img></img>
+        <h1>{ this.state.data.title }</h1>
+        <h2>{ this.state.data.author }</h2>
+        <h3>{ this.state.data.url }</h3>
+        <Quote url='http://tesquotes.kenjin.me/api/random_quote'/>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <Content />,
    document.getElementById('content')
 );
