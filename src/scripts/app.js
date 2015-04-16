d8096d05278eb1d2738d0fb5e4c3660f8de5c34e
Kenneth <kenjin.p@gmail.com>
2015-04-16 15:23:17 +0200

    reconfigured react according to think-react article, added styling

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,99 +1,87 @@
+//Quote Viewer, shows quotes with a background.
+
+//QuoteViewer
+  // * Background
+        //--title author
+  // * Quote
+        //-- quote author load
+
+// dependencies
 global.jQuery = global.$ = require('jquery');
+
 var React = require('react'),
     imagesLoaded = require('imagesloaded');
-var name = 'Quotes From Tamriel';
-var quote = {};
-var redditData = {
-  title:'',
-  author:'',
-  url:''
-};
+
+//React Components
+
+var Background = React.createClass({
+  render: function() {
+    var title = this.props.back.title;
+    var author = this.props.back.author;
+    var url = this.props.back.url;
+    return (
+      <div className="background">
+        <img></img>
+        <h3>{title}</h3>
+        <h4>{author}</h4>
+      </div>
+    );
+  }
+});
 
 var Quote = React.createClass({
-  loadQuoteFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      sucess: function(data) {
-        //after returned data
-        this.setState({data: data});
-        console.log('ajaxing');
-        console.log(data);
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  getInitialState: function() {
-    //initial data
-    return {data: quote};
-  },
-  componentDidMount: function(){
-    this.loadQuoteFromServer();
-  },
   render: function() {
+    var author = this.props.quote.author;
+    var quote = this.props.quote.quote;
     return (
-      <div>
-        {this.state.data}
+      <div className="quote-card">
+        <h1>{quote}</h1>
+        <p>{author}</p>
       </div>
     );
   }
 });
 
-var Content = React.createClass({
-  loadBackgroundFromReddit: function() {
-    var that = this;
-    reddit.hot('skyrimporn').limit(100).fetch(function (res) {
-      function grabRandom (res) {
-        var randomInt = Math.floor(Math.random() * res.data.children.length);
-        var postData = res.data.children[randomInt].data;
-        var extList = postData.url.split('.');
-        var ext = extList[extList.length - 1];
-        if ( ext !== 'jpg') {
-          return grabRandom(res);
-        } else {
-          return postData;
-        }
-      }
-      redditData = grabRandom(res);
-      $('img').prop('src', redditData.url);
-      $('img').imagesLoaded(function() {
-        $('img').show();
-        that.setState({data: redditData});
-      });
-    });
-  },
-  onEnter: function() {
-    var that = this;
-    $(window).keypress(function(e) {
-      if (e.keyCode == 0 || e.keyCode == 32) {
-        that.loadBackgroundFromReddit();
-      }
-    });
-  },
+var QuoteViewer = React.createClass({
   getInitialState: function() {
-    return {data: redditData};
+    //returns initial state
+    return {
+      back: BACKGROUND,
+      quote: QUOTE
+    }
   },
-  componentDidMount: function() {
-    $('img').hide();
-    this.onEnter();
-    this.loadBackgroundFromReddit();
+  handleUserInput: function() {
+    //change state data based on input
+  },
+  getBackgroundFromReddit: function() {
+    //get data from reddit
+  },
+  getQuoteFromTESQuotes: function() {
+    //get data from api
   },
   render: function() {
     return (
       <div>
-        <img></img>
-        <h1>{ this.state.data.title }</h1>
-        <h2>{ this.state.data.author }</h2>
-        <h3>{ this.state.data.url }</h3>
-        <Quote url='http://tesquotes.kenjin.me/api/random_quote'/>
+        <Background back={this.state.back}/>
+        <Quote quote={this.state.quote}/>
       </div>
     );
   }
 });
 
+//Mock Up Data
+var QUOTE = {
+  quote: 'I used to be an adventurer, but then I took an arrow to the knee',
+  author: 'Guard'
+},
+BACKGROUND = {
+  title:'The Ritual Stone',
+  author:'ESloman',
+  url:'http://i.imgur.com/8AZQPfI.jpg'
+};
+
+//Render
 React.render(
-  <Content />,
-   document.getElementById('content')
+  <QuoteViewer/>,
+  document.getElementById('content')
 );
